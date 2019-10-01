d16157c485d5974eda0807e69cd0d69588aca3e2
Martin Anders <martin.anders.external@zalando.de>
2019-10-01 18:23:58 +0200

    chnage funding and footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -204,8 +204,7 @@ const Layout = ({ children, customSEO }) => {
         <GlobalStyle />
         {children}
         <Footer>
-          &copy; 2019 by LekoArts. All rights reserved. <br />
-          <a href="https://github.com/LekoArts/gatsby-starter-minimal-blog">GitHub Repository</a> <br />
+          &copy; 2019 by Martin Anders. <br />
           <span>Last build: {buildTime}</span>
         </Footer>
       </>
